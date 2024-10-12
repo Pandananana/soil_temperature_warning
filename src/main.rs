e7e3ba49c9d49cffe7031ec0a5adc7b61b2baa88
Pandananana <andreaswendelboe.dk@gmail.com>
2024-10-12 15:45:17 +0200

    Moved driver init to function

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,30 @@ use std::time::Duration;
 use thirtyfour::prelude::*;
 
 fn start_geckodriver() -> Result<Child, std::io::Error> {
-    Command::new("geckodriver")
+    let geckodriver = Command::new("geckodriver")
         .arg("--port")
         .arg("4444")
-        .spawn()
+        .spawn();
+
+    // Wait a moment for geckodriver to be ready
+    thread::sleep(Duration::from_secs(2));
+    geckodriver
+}
+
+async fn start_browser() -> WebDriverResult<WebDriver> {
+    let caps = DesiredCapabilities::firefox();
+    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+    Ok(driver)
 }
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
-    // Start geckodriver
+    // Start browser
     let mut geckodriver = start_geckodriver().expect("Failed to start geckodriver");
-    
-    // Wait a moment for geckodriver to be ready
-    thread::sleep(Duration::from_secs(2));
-
-    let caps = DesiredCapabilities::firefox();
-    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+    let driver = start_browser().await?;
 
     // Your test code here
-    driver.goto("https://wikipedia.org").await?;
+    driver.goto("https://weathermodels-plant.dlbr.dk/(S(gzy2tilppcazluhtlh4hleft))/default.aspx").await?;
 
     // Wait a moment for geckodriver to be ready
     thread::sleep(Duration::from_secs(2));
